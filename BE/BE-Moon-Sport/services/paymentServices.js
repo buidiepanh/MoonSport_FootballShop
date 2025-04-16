3e9c889d5e9259fb9033cad17ec61f28268542b4
BuiDiepAnh <buidiepanh0905@gmail.com>
2025-04-16 14:30:18 +0700

    payment success

--- BE/BE-Moon-Sport/services/paymentServices.js
+++ BE/BE-Moon-Sport/services/paymentServices.js
@@ -28,23 +28,22 @@ const createVNPayUrl = (req, res) => {
     vnp_Locale: "vn",
     vnp_CurrCode: "VND",
     vnp_TxnRef: orderId,
-    vnp_OrderInfo: "Thanh toan don hang #" + orderId,
+    vnp_OrderInfo: `Thanh toan don hang ${orderId}`,
     vnp_OrderType: "other",
     vnp_Amount: amount * 100,
     vnp_ReturnUrl: returnUrl,
     vnp_IpAddr: ipAddr,
     vnp_CreateDate: createDate,
-    vnp_BankCode: bankCode || "NCB",
+    vnp_BankCode: bankCode || "VNBANK",
   };
 
   vnp_Params = sortObject(vnp_Params);
-
-  const signData = qs.stringify(vnp_Params, { encode: false });
+  const signData = new URLSearchParams(vnp_Params).toString();
   const hmac = crypto.createHmac("sha512", secretKey);
   const signed = hmac.update(Buffer.from(signData, "utf-8")).digest("hex");
 
   vnp_Params["vnp_SecureHash"] = signed;
-  const paymentUrl = `${vnpUrl}?${qs.stringify(vnp_Params, { encode: false })}`;
+  const paymentUrl = `${vnpUrl}?${qs.stringify(vnp_Params, { encode: true })}`;
 
   res.json({ url: paymentUrl });
 };
@@ -57,23 +56,25 @@ const vnpayReturn = (req, res) => {
   delete vnp_Params["vnp_SecureHashType"];
 
   const secretKey = config.get("vnp_HashSecret");
-  const signData = qs.stringify(sortObject(vnp_Params), { encode: false });
+  const signData = new URLSearchParams(vnp_Params).toString();
   const hmac = crypto.createHmac("sha512", secretKey);
   const signed = hmac.update(Buffer.from(signData, "utf-8")).digest("hex");
 
   if (secureHash === signed) {
-    res.send("Thanh toán thành công!");
+    res.status(200).json("Thanh toán thành công!");
   } else {
-    res.send("Chữ ký không hợp lệ!");
+    res.status(400).json("Chữ ký không hợp lệ!");
   }
 };
 
 function sortObject(obj) {
-  let sorted = {};
-  let keys = Object.keys(obj).sort();
-  keys.forEach((key) => {
-    sorted[key] = obj[key];
-  });
+  const sorted = {};
+  const keys = Object.keys(obj).sort();
+  for (let key of keys) {
+    if (Object.prototype.hasOwnProperty.call(obj, key)) {
+      sorted[key] = obj[key];
+    }
+  }
   return sorted;
 }
 
